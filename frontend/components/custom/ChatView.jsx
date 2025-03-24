78fa6ef6d1cf1a72639adad959e1acc3c0414bdf
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-03-24 18:16:49 +0530

    fixing the error of the map not working

--- frontend/components/custom/ChatView.jsx
+++ frontend/components/custom/ChatView.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client"
+import { MessagesContext } from "@/context/MessagesContext";
+import { api } from "@/convex/_generated/api";
+import { useConvex } from "convex/react";
+import { useParams } from "next/navigation";
+import React, { useContext, useEffect } from "react";
 
 function ChatView() {
-  return (
-    <div>ChatView</div>
-  )
+    const { id } = useParams();
+    const convex = useConvex();
+    const { messages, setMessages } = useContext(MessagesContext);
+
+    useEffect(() => {
+        id && GetWorkspaceData();
+    }, [id])
+
+    //   Used to Get workspace data from the workspace id
+
+    const GetWorkspaceData = async () => {
+        const result = await convex.query(api.workspace.GetWorkspace, {
+            workspaceId: id,
+        });
+        setMessages(result?.messages);
+    };
+    return (
+        <div>
+            <div>
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        <h2>{msg.content}</h2>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default ChatView
+export default ChatView;
